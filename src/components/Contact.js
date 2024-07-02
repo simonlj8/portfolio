68def2eff124aec1ec9ad299d494cc6c16882c62
Simon Ljungström <simonlj8@gmail.com>
2024-07-02 17:58:37 +0200

    Change favicon icon and some textchanges

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -59,9 +59,9 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a className="text-indigo-400 leading-relaxed"><a href="mailto:simonlj8@gmail.com">
                 simonlj8@gmail.com
-              </a>
+              </a></a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
--- src/data.js
+++ src/data.js
@@ -3,23 +3,23 @@ export const projects = [
     title: "Wordle game",
     subtitle: "MERN Stack",
     description:
-      "Ett wordle inspirerat spel",
-    image: "./project-1.gif",
+      "A wordle inspired game",
+    image: "./bild1.png",
     link: "https://github.com/simonlj8/wordle_game",
   },
   {
     title: "Pong",
     subtitle: "Java",
     description:
-      "Ett litet pong spel skrivet i java",
-    image: "./project-2.gif",
+      "A pong game written in java",
+    image: "./bild2.png",
     link: "https://github.com/simonlj8/Game",
   },
   {
-    title: "Parkering app",
+    title: "Parking app",
     subtitle: "Java, Springboot",
     description:
-      "Parkerings app",
+      "Backend parking app",
     image: "./project-3.gif",
     link: "https://github.com/simonlj8/parkering",
   },
@@ -27,7 +27,7 @@ export const projects = [
     title: "Hacker Escape Room",
     subtitle: "JavaScript, HTLM, CSS",
     description:
-      "Bokningssida för hacker escape room",
+      "Escape room booking page",
     image: "./bild4.png",
     link: "https://esc-room-omega.vercel.app",
   },
